61c76e1455e71acda078ddf012d4c06f2007ae08
Maxim <chemaxa@gmail.com>
2015-02-26 13:58:59 +0300

    add youtube.js + remove jwplayer & service functions

--- app.js
+++ app.js
@@ -11,12 +11,18 @@ function go() {
     var inputBroadcastUrl = document.querySelector('.broadcastUrl');
     var divBroadcastsList = document.querySelector('.broadcastsList');
 
-    var myStream, myStreamData;
+    var myStream,
+        myStreamData,
+        playerConfig;
 
 
     setBroadcastForm.addEventListener('submit', setNewBroadcast, false);
     btnGetBroadcats.addEventListener('click', getBroadcastList, false);
 
+    playerConfig = {
+        "techOrder": ["youtube"],
+        "src": "http://www.youtube.com/watch?v=xjS6SftYQaQ"
+    };
 
 
     ////////////// WORK WITH PLAYER //////////////////
@@ -29,9 +35,7 @@ function go() {
     }
 
     function setPlayerConfig(conf) {
-        jwplayer('player').setup({
-            file: conf.file
-        });
+        divPlayer.dataset.setup = JSON.stringify(playerConfig);
     }
 
     function getPlayerConfig() {
@@ -40,27 +44,9 @@ function go() {
 
 
 
-    jwplayer(divPlayer).setup({
-        primary: "html5",
-        file: 'http://www.youtube.com/watch?v=SfhV0Sew0j4',
-        title: 'Ленинград — СИЗОнная'
-    });
 
 
-    jwplayer(divPlayer).onPause(function (event) {
-        console.log(1, event.newstate);
-        myStreamData.state = event.newstate;
-        writeDataToDB(myStream, myStreamData);
-        console.log('state on Pause', myStream.key(), myStreamData);
-    });
 
-    jwplayer(divPlayer).onPlay(function (event) {
-        console.log(2, event.newstate, this.getPosition());
-        myStreamData.state = event.newstate;
-        myStreamData.position = this.getPosition();
-        writeDataToDB(myStream, myStreamData);
-        console.log('state on Play', myStream.key(), myStreamData);
-    });
 
 
 
@@ -176,41 +162,6 @@ function go() {
     }
 
 
-
-    //*************  SERVICE FUNCTIONS !!!!! **********************//
-
-    // trim spaces
-    /*function trim(string) {
-        return string.replace(/(^\s+)|(\s+$)/g, "");
-    }*/
-
-    //Check valid Url https, http or ftp
-    /*function isValidUrl(url) {
-        var template = /^(?:(?:https?|http|ftp):\/\/(?:[a-z0-9_-]{1,32}(?::[a-z0-9_-]{1,32})?@)?)?(?:(?:[a-z0-9-]{1,128}\.)+(?:com|net|org|mil|edu|arpa|ru|gov|biz|info|aero|inc|name|[a-z]{2})|(?!0)(?:(?!0[^.]|255)[0-9]{1,3}\.){3}(?!0|255)[0-9]{1,3})(?:\/[a-z0-9.,_@%&?+=\~\/-]*)?(?:#[^ \'\"&<>]*)?$/i;
-        var regex = new RegExp(template);
-        return (regex.test(url) ? true : false);
-    }*/
-
-    /*   function compareStreams(streamA, streamB) {
-
-           return streamA.lastTimeModificated - streamB.lastTimeModificated;
-
-       }*/
-
-
-    /* function debugData(data) {
-         var li = document.createElement('li');
-         if (data instanceof Object) {
-             for (var key in data) {
-                 li.innerHTML += '<br>' + key + ' ' + data[key];
-             }
-             console.log(data);
-         } else {
-             li.innerHTML = data;
-             console.log(1, data);
-         }
-         log.appendChild(li);
-     }*/
 }
 
 document.addEventListener("DOMContentLoaded", function () {
