98ddc2b0671557c527596ffd9c379875cd32715c
Chemaxa <chemaxa@gmail.com>
2015-09-02 12:59:39 +0300

    Изменил формы ввода урл видео
    Добавлены формы бутстрапа для ввода и получения урл на видео

--- js/app.js
+++ js/app.js
@@ -1,10 +1,7 @@
-function go() {
+$(function() {
     'use strict';
-    var broadcastsListRef = new Firebase('https://fiery-heat-9055.firebaseio.com/broadcasts');
-    var streamsListRef = new Firebase('https://fiery-heat-9055.firebaseio.com/streams');
-
-    var setBroadcastForm = document.querySelector('.setBroadcastForm');
-    var btnGetBroadcats = document.getElementsByName('btnGetBroadcasts')[0];
+    var broadcastsListRef = new Firebase('https://fiery-heat-9055.firebaseio.com/broadcasts'),
+        streamsListRef = new Firebase('https://fiery-heat-9055.firebaseio.com/streams');
 
     //var player = document.getElementById('player');
 
@@ -23,22 +20,18 @@ function go() {
     var player = videojs('player', playerConfig);
 
 
-
-    setBroadcastForm.addEventListener('submit', setNewBroadcast, false);
-    btnGetBroadcats.addEventListener('click', getBroadcastList, false);
-
-
+    // Set Broadcast Url Button
+    setBroadcast.addEventListener('click', setNewBroadcast, false);
+    getBroadcasts.addEventListener('click', getBroadcastList, false);
 
 
     ////////////// WORK WITH PLAYER //////////////////
-
-
 
     function setPlayerConfig(conf) {
 
         console.log(2, conf);
 
-        broadcastsListRef.orderByKey().equalTo(conf.broadcastId).on("child_added", function (snapshot) {
+        broadcastsListRef.orderByKey().equalTo(conf.broadcastId).on("child_added", function(snapshot) {
             console.log(snapshot.val().src);
             player.src(snapshot.val().src);
         });
@@ -51,8 +44,6 @@ function go() {
 
     };
 
-
-
     function logPlayerState() {
         myStreamData.state = 'play';
         myStreamData.position = player.currentTime();
@@ -60,7 +51,7 @@ function go() {
         if (player.paused()) {
             myStreamData.state = 'pause';
             if (myStream != undefined) {
-                broadcastsListRef.orderByKey().equalTo(myStreamData.broadcastId).on("child_added", function (snapshot) {
+                broadcastsListRef.orderByKey().equalTo(myStreamData.broadcastId).on("child_added", function(snapshot) {
                     console.log(snapshot.key(), myStream.key());
                     //writeDataToDB(snapshot.ref(), );
                 });
@@ -86,11 +77,9 @@ function go() {
     /////////// WORK WITH HTML ////////////////////
 
     function addBroadcastToListCallback(broadcastsList) {
-        var ul = divBroadcastsList.getElementsByTagName('ul')[0];
-        if (ul.childNodes.length > 1) {
-            divBroadcastsList.removeChild(ul);
-            ul = document.createElement('ul');
-            divBroadcastsList.appendChild(ul);
+        // Clear Broadcast List
+        if (broadcasts.children.length) {
+            broadcasts.innerHTML = '';
         }
 
 
@@ -98,8 +87,8 @@ function go() {
             var li = document.createElement('li');
             var a = document.createElement('a');
 
-            (function (broadcastId) {
-                a.addEventListener('click', function () {
+            (function(broadcastId) {
+                a.addEventListener('click', function() {
                     getCurrentBroadcast(broadcastId);
                 }, false);
             })(key);
@@ -108,12 +97,12 @@ function go() {
             //a.innerHTML = broadcastsList[key].url;
             a.innerHTML = key + '<br>' + broadcastsList[key].src;
             li.appendChild(a);
-            ul.appendChild(li);
+            broadcasts.appendChild(li);
         }
     }
 
     function broadcastsList() {
-        broadcastsListRef.once('value', function (dataSnapshot) {
+        broadcastsListRef.once('value', function(dataSnapshot) {
             addBroadcastToListCallback(dataSnapshot.val());
         });
     }
@@ -149,7 +138,7 @@ function go() {
 
     function updateStreamDataFromLastAliveStream() {
         // GET CONF FROM LAST ALIVE STREAM FOR CURRENT BROADCAST
-        streamsListRef.orderByChild("lastTimeModificated").on("child_added", function (dataSnapshot) {
+        streamsListRef.orderByChild("lastTimeModificated").on("child_added", function(dataSnapshot) {
             if (myStream.key() != dataSnapshot.key() && myStreamData.broadcastId == dataSnapshot.val().broadcastId)
                 updateStreamData(dataSnapshot);
         });
@@ -173,22 +162,25 @@ function go() {
         //broadcastsListRef.remove();
         //streamsListRef.remove();
         //////////////////////////////////////
+        var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+        var regex = new RegExp(expression);
 
-        event.preventDefault();
 
-        if (event.target[0].value != '') {
-            var newBroadcastRef = broadcastsListRef.push();
-            var u = new URL(event.target[0].value);
-            var newBroadcastData = {
-                src: event.target[0].value,
-                streamId: 0,
-                techOrder: u.host
-            };
+        if (inputPutUrl.value && regex.test(inputPutUrl.value)) {
+            var newBroadcastRef = broadcastsListRef.push(),
+                u = new URL(inputPutUrl.value),
+                newBroadcastData = {
+                    src: inputPutUrl.value,
+                    streamId: 0,
+                    techOrder: u.host
+                };
 
             writeDataToDB(newBroadcastRef, newBroadcastData);
             //getBroadcast();
-            inputBroadcastUrl.value = event.target[0].value;
+            inputGetUrl.value = inputPutUrl.value;
+            return;
         }
+        alert('Введите УРЛ');
     }
 
     function setNewStreamRef() {
@@ -202,11 +194,4 @@ function go() {
     function deleteDataFromDB(ref) {
         ref.remove();
     }
-
-
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-    'use strict';
-    go();
 });
