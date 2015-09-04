0d0ad8c88af95dae6ea364836a9fd65c0df48180
Chemaxa <chemaxa@gmail.com>
2015-09-04 12:59:43 +0300

    Edit OOP

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 $(function() {
     'use strict';
+    // Firebase Refs
     var broadcastsListRef = new Firebase('https://fiery-heat-9055.firebaseio.com/broadcasts'),
         streamsListRef = new Firebase('https://fiery-heat-9055.firebaseio.com/streams'),
         // Html Elements
         broadcasts = document.getElementById('broadcasts'),
-        // Stream link
-        myStream,
+        inputPutUrl = document.getElementById('inputPutUrl'),
+        // Stream 
+        myStreamRef,
         // Stream state object
         myStreamData = {},
 
@@ -16,23 +18,24 @@ $(function() {
         },
 
         //VideoJS Player Object
-        player = videojs('player', playerConfig);
+        player = videojs('player', playerConfig),
+
+        //Create  instances of :
+        // Broadcast Controller
+        brdCntrl = new BrdCntr(),
+        // Player Controller
+        plrCntrl = new PlCntr(),
+        // Stream Controller
+        strCntrl = new StrCntr();
 
 
-    // Set Broadcast Url Button
-    setBroadcast.addEventListener('click', setNewBroadcast, false);
+    // Add events
+    setBroadcast.addEventListener('click', brdCntrl.set, false);
     getBroadcasts.addEventListener('click', getBroadcastList, false);
+    //setInterval(PlrCntr.log, 1000);
+    //setInterval(PlCntr.set, 1000);
 
-    //Create  instances of :
-    // Broadcast Controller
-    brdCntrl = new BrdCntr();
-    // Player Controller
-    plrCntrl = new PlCntr();
-    // Stream Controller
-    strCntrl = new StrCntr();
-
-    ////////////// WORK WITH PLAYER //////////////////
-    // Constructor PLayer
+    // PLayer Constructor 
     function PlrCntr() {
         this.set = function(conf) {
 
@@ -57,18 +60,14 @@ $(function() {
                 myStreamData.state = 'pause';
             }
 
-            if (myStream != undefined) {
-                myStream.set(myStreamData);
+            if (myStreamRef != undefined) {
+                myStreamRef.set(myStreamData);
             }
         }
 
     };
 
-
-    //setInterval(PlrCntr.log, 1000);
-    //setInterval(PlCntr.set, 1000);
-
-    /////////// WORK WITH HTML ////////////////////
+    //Stream Controller 
     function StrCntr() {
         this.updateStreamData = function(streamData) {
             myStreamData.state = streamData.val().state;
@@ -77,54 +76,55 @@ $(function() {
             plrCntrl.set(myStreamData);
         }
 
-        this.setNewStreamRef = function() {
+        this.setRef = function() {
             return streamsListRef.push();
         }
+
     }
 
+    // Broadcast Controller
     function BrdCntr() {
-        this.addToList = function(broadcastsList) {
-            // Clear Broadcast List
-            if (broadcasts.children.length) {
-                broadcasts.innerHTML = '';
-            }
-
-            for (var key in broadcastsList) {
-                var a = document.createElement('a');
-                a.classList.add('list-group-item');
-
-                (function(broadcastId) {
-                    a.addEventListener('click', function() {
-                        getCurrentBroadcast(broadcastId);
-                    }, false);
-                })(key);
-
-                a.href = 'javascript:void(0)';
-                //a.innerHTML = broadcastsList[key].url;
-                a.innerHTML = key + '<br>' + broadcastsList[key].src;
-
-                broadcasts.appendChild(a);
-            }
-        }
 
         // Create/Update list of Broadcasts
-        this.broadcastsList = function() {
+        this.List = function() {
             broadcastsListRef.once('value', function(dataSnapshot) {
-                this.addToList(dataSnapshot.val());
-            });
+
+                // Clear Broadcast List
+                if (broadcasts.children.length) {
+                    broadcasts.innerHTML = '';
+                }
+
+                for (var key in dataSnapshot.val()) {
+                    var a = document.createElement('a');
+                    a.classList.add('list-group-item');
+
+                    (function(broadcastId) {
+                        a.addEventListener('click', function() {
+                            this.getCurrent(broadcastId);
+                        }, false);
+                    })(key);
+
+                    a.href = 'javascript:void(0)';
+                    //a.innerHTML = dataSnapshot.val()[key].url;
+                    a.innerHTML = key + '<br>' + dataSnapshot.val()[key].src;
+
+                    broadcasts.appendChild(a);
+                }
+
+            })
         }
 
-        this.getCurrentBroadcast = function(broadcastId) {
-            // CREATE NEW BROADCAST & STREAM
-            if (!myStream) {
-                myStream = setNewStreamRef();
+        this.getCurrent = function(broadcastId) {
+            // CREATE NEW STREAM
+            if (!myStreamRef) {
+                myStreamRef = strCntr.setRef();
                 myStreamData = {
                     'state': 'pause',
                     'position': 0,
                     'broadcastId': broadcastId
                 };
-                myStream.set(myStreamData);
-                console.log('CREATE NEW STREAM', myStream.key(), myStreamData);
+                myStreamRef.set(myStreamData);
+                console.log('CREATE NEW STREAM', myStreamRef.key(), myStreamData);
             }
 
             // CHANGE CURRENT BROADCAST
@@ -134,39 +134,38 @@ $(function() {
                     'position': 0,
                     'broadcastId': broadcastId
                 };
-                myStream.set(myStreamData);
+                myStreamRef.set(myStreamData);
             }
         }
 
-    }
+        this.set = function() {
+            ///// ONLY FOR DEBUG!!!!!!! THIS CLEAR ALL DATA IN DB broadcast LIST !
+            //broadcastsListRef.remove();
+            //streamsListRef.remove();
+            //////////////////////////////////////
 
-    function setNewBroadcast() {
-        ///// ONLY FOR DEBUG!!!!!!! THIS CLEAR ALL DATA IN broadcast LIST !
-        //broadcastsListRef.remove();
-        //streamsListRef.remove();
-        //////////////////////////////////////
-        var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
-        var regex = new RegExp(expression);
+            // Checking input URL
+            var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi,
+                regex = new RegExp(expression);
 
-
-        if (inputPutUrl.value && regex.test(inputPutUrl.value)) {
-            var newBroadcastRef = broadcastsListRef.push(),
-                u = new URL(inputPutUrl.value),
-                newBroadcastData = {
-                    src: inputPutUrl.value,
-                    streamId: 0,
-                    techOrder: u.host
-                };
-
-            dbCntrl.write(newBroadcastRef, newBroadcastData);
-            //getBroadcast();
-            inputGetUrl.value = inputPutUrl.value;
-            return;
+            // Validate inputed URL
+            if (regex.test(inputPutUrl.value)) {
+                //Create new Broadcast
+                var newBroadcastRef = broadcastsListRef.push(),
+                    u = new URL(inputPutUrl.value),
+                    newBroadcastData = {
+                        src: inputPutUrl.value,
+                        streamId: 0,
+                        techOrder: u.host
+                    };
+                // Write created broadcast to DB
+                dbCntrl.write(newBroadcastRef, newBroadcastData);
+                // Set inputed URL value for copy
+                inputGetUrl.value = inputPutUrl.value;
+                return;
+            }
+            // If URL is not validate
+            alert('Введите ссылку на видео...');
         }
-        alert('Введите УРЛ');
     }
-
-
-
-
 });
